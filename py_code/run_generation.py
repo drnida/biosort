3353965f15643a564e52492b7e37ede024be7544
John Schroeder <js22@pdx.edu>
2014-02-28 18:06:04 -0800

    removed some prints

--- py_code/run_generation.py
+++ py_code/run_generation.py
@@ -235,8 +235,6 @@ def Setup_Gen(folders, rfolders, arraylist, env):
    
    #Set new pressure
    num = labtable[env.breeders-1].avgops*env.buff
-   print "\nNum: "+str(num)
-   print "env.pressure: "+str(env.pressure)
    if num < env.pressure:
        env.pressure = num
    #Put orgs in correct folders for breeders
