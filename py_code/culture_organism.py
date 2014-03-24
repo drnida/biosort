0bb650b968532f47c6ef25e8328380dec723a334
Michael Valentine <ValentineHypothesis@gmail.com>
2014-03-24 16:41:33 -0700

    Finished commenting and cleaning up culture_organism.py

--- py_code/culture_organism.py
+++ py_code/culture_organism.py
@@ -1,9 +1,7 @@
 from transcribe_c import writec
-from subprocess import Popen, PIPE
-import subprocess
+from subprocess import Popen, PIPE, call
 
 
-#Imporatnt, next step is to separate this up into 2 functions
 #One to set up the file and compile it
 #The other to run it
 #This is so that we can run a setup loop in rungen and then a test loop
@@ -21,7 +19,7 @@ import subprocess
 def testgene(org, arraylist, env):
     #Creates organism.cpp file and compiles it
     writec("./habitat/" +org.folder, org, env.pressure)
-    subprocess.call('g++ ./habitat/' + org.folder + '*.cpp ./habitat/biosort.o -o ./habitat/' + org.folder + 'organism.out -g', shell = True) 
+    call('g++ ./habitat/' + org.folder + '*.cpp ./habitat/biosort.o -o ./habitat/' + org.folder + 'organism.out -g', shell = True) 
     
 
     total_opcount = 0 #Sum of all individual opcounts   
@@ -29,9 +27,9 @@ def testgene(org, arraylist, env):
     for i in range(env.runs):
         array = ' '.join(str(x) for x in arraylist[i]) #Pulls array to sort from the given arraylist.
         command = './habitat/' + org.folder + 'organism.out '+array #Command to run organism on array from the command line
-        fileout = subprocess.Popen(command, stdin = PIPE, stdout = PIPE, stderr = PIPE, bufsize = 1, shell = True) #Retrieves the operations performed by the C++ before it ended 
+        fileout = Popen(command, stdin = PIPE, stdout = PIPE, stderr = PIPE, bufsize = 1, shell = True) #Retrieves the operations performed by the C++ before it ended 
       
-        # Gets opcount and adding to total_opcount 
+        # Gets opcount and adds to total_opcount 
         ops = fileout.stdout.readline() 
         org.ops.append(int(ops)) 
       
@@ -41,7 +39,6 @@ def testgene(org, arraylist, env):
         total_opcount += org.ops[i]
         
      
-    # Calculating mean number of ops 
+    # Calculates mean number of ops 
     org.avgops = total_opcount / env.runs 
-    print "Ops: " + str(org.avgops)
     return
