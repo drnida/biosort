29b95a9026b5b4130db3f3860d6a96cca843348b
John Schroeder <js22@pdx.edu>
2014-03-24 17:13:37 -0700

    Added prompt in case of pre-existing log files to biosort.sh

--- biosort.sh
+++ biosort.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 #Starts python program and cleans up after it
-rm ./log/log* 2> /dev/null
+
+if [ -e ./logs/log* ]
+then
+    read -p "Do you wish do delete pre-existing log files? " response
+    case $response in
+            [Yy]* ) rm ./log/log* 2> /dev/null;;
+            [Nn]* ) echo "Exiting..."; exit;;
+            * ) echo "Please respond yes or no";;
+        esac
+fi
 
 python py_code/biosort.py
 
